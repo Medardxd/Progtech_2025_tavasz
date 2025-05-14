56d03d76bee5ab1d906699e0cccb562e1184d603
Medardxd <114134902+Medardxd@users.noreply.github.com>
2025-05-14 10:11:41 +0200

    style: Cleaning up the UI

--- car-rental/car-rental/src/main/java/com/carrental/App.java
+++ car-rental/car-rental/src/main/java/com/carrental/App.java
@@ -2,17 +2,26 @@ package com.carrental;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(
-                FXMLLoader.load(getClass().getResource("/com/carrental/view/login.fxml"))
-        ));
+        Parent root = FXMLLoader.load(getClass().getResource(
+                "/com/carrental/view/login.fxml"));
+
+        Scene scene = new Scene(root, 300, 250);    // width=300, height=250
+        scene.getStylesheets().add(
+                getClass().getResource("/com/carrental/style/styles.css")
+                        .toExternalForm()
+        );
+
+
+        stage.setScene(scene);
         stage.setTitle("Car-Rental • Login");
+        stage.setResizable(false);
         stage.show();
     }
-    public static void main(String[] args) { launch(); }
 }
